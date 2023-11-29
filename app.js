95d820fa32d739a7b781b4ff46922bf1ead1bfa8
고경남 <rhrudska987@naver.com>
2023-11-29 19:05:48 +0900

    build: 프로젝트 세팅 및 서버 구축

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express")
+const http = require("http")
+const app = express();                                              //express를 실행한 내용을 app에 담음
+const path = require("path")
+const server = http.createServer(app) //서버 생성
+const socketIO = require("socket.io")
+
+const io = socketIO(server);
+
+app.use(express.static(path.join(__dirname, "src")))               //__dirname은 프로젝트 폴더, path.join은 운영체제마다 /와\가 다르기에 사용
+const PORT = process.env.PORT || 5000;
+
+io.on("connection", (socket) => {
+    console.log("connect")
+})
+
+server.listen(PORT, () => console.log(`server is running ${PORT}`))
