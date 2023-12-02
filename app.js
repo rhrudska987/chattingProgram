9b7fe62be3b83a85b9943a36512a0cce965a01bd
고경남 <rhrudska987@naver.com>
2023-12-02 20:46:31 +0900

    feat: 채팅 기능 개발
    html, css 파일 수정
    
    socket기능 수정

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express();                                              //express를
 const path = require("path")
 const server = http.createServer(app) //서버 생성
 const socketIO = require("socket.io")
+const moment = require("moment")
 
 const io = socketIO(server);
 
@@ -13,7 +14,12 @@ const PORT = process.env.PORT || 5000;
 io.on("connection", (socket) => {
     console.log("connect")
     socket.on("ChatRoom1", (data)=>{
-        io.emit("ChatRoom1", data)
+        const {name, message} = data;
+        io.emit("ChatRoom1", {
+            name: name,
+            message: message,
+            time: moment(new Date()).format("h:mm A")
+        })
     })
 })
 
--- src/js/chat.js
+++ src/js/chat.js
@@ -5,20 +5,51 @@ const nickname = document.querySelector("#nickname")
 const chatList = document.querySelector(".chatting-list")
 const chatInput = document.querySelector(".chatting-input")
 const sendButton = document.querySelector(".send-button")
+const displayContainer = document.querySelector(".display-container")
 
-sendButton.addEventListener("click", ()=>{
+chatInput.addEventListener("keypress", (event)=>{
+    if(event.keyCode === 13) {
+        send()
+    }
+})
+
+function send() {
     const param = {
         name: nickname.value,
         message: chatInput.value
     }
     socket.emit("ChatRoom1", param)
-})
+}
+
+sendButton.addEventListener("click", send)
 
 
 socket.on("ChatRoom1", (data) => {
-    const li = document.createElement("li");
-    li.innerText = `${data.name}님이 - ${data.message}`;
-    chatList.appendChild(li)
+    const {name, message, time} = data;
+    const item = new LiModel(name, message, time);
+    item.makeLi()
+    displayContainer.scrollTo(0, displayContainer.scrollHeight)
 })
+
+function LiModel(name, message, time) {
+    this.name = name;
+    this.message = message;
+    this.time = time;
+
+    this.makeLi = ()=> {
+        const li = document.createElement("li");
+        li.classList.add(nickname.value === this.name ? "sent" : "received")
+        // https://play.google.com/store/apps/details?id=com.kaovx.tf1.golfaround&hl=en_US
+        // <img class="image" src="https://placeimg.com/50/50/any" alt="any">
+        const dom = `<span class="profile">
+        <span class="user">${this.name}</span>
+        <img class="image" src="https://play-lh.googleusercontent.com/vqOAck2RIZoJsOO6eOBwnXgPkwVW0IL7n3mqO-dqYvp2xJ5HH3AcuIp3ChI7FWQ7YX8=w480-h960-rw" alt="profile Image">
+        </span>
+        <span class="message">${this.message}</span>
+        <span class="time">${this.time}</span>`;
+        li.innerHTML = dom;
+        chatList.appendChild(li)
+    }
+}
 
 console.log(socket)
